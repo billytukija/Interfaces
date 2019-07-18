4abfc35faf742474d965ca62964a1ba606f7d461
Billy Tukija Richard <billytukija@gmail.com>
2019-07-18 17:16:53 -0300

    Interface
    Simple way to use interfaces in C#

---
+++ Interfaces/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public interface People {
+        void Create();
+        void Delete();
+        void Edit();
+
+        object Find();
+
+    }
+    
+    public class Professor : People
+    {
+        public void Create()
+        {
+            // Rules
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            // Rules
+            throw new NotImplementedException();
+        }
+
+        public void Edit()
+        {
+            // Rules
+            throw new NotImplementedException();
+        }
+
+        public object Find()
+        {
+            // Rules
+            throw new NotImplementedException();
+        }
+    }
+    public class Pupil : People
+    {
+        public void Create()
+        {
+            // Rules
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            // Rules
+            throw new NotImplementedException();
+        }
+
+        public void Edit()
+        {
+            // Rules
+            throw new NotImplementedException();
+        }
+
+        public object Find()
+        {
+            // Rules
+            throw new NotImplementedException();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
